2de3cb2b27c1ccc02566be2456c72b0ea9aa2d3e
shreya5art <ayushr4j@gmail.com>
2025-11-22 00:57:22 +0530

    log

--- include/core/log/log.hpp
+++ include/core/log/log.hpp
@@ -7,48 +7,56 @@ namespace expp{
 
     namespace log{
 
+        enum class LogLevel : uint8_t{
+            Debug, Info, Warn, Error
+        };
         
         /**
          * @brief Logger class provides the base for Debug logging
          * 
-         * This class 
+         * 
          */
         class Logger{
             
             public:
 
-                virtual void log(const char* logString){
+                virtual void log(LogLevel level, const char* logString){
                     std::cerr << logString;
                 }
 
-                virtual void info(const char* logString){
-                    std::cerr << logString;
-                }
-                
-                virtual void error(const char* logString){
-                    std::cerr << logString;
-                }
-                
         };
 
-        Logger* instance;
+        Logger* instance = new Logger();
 
         template <typename Arg, typename... Args>
+        void log(LogLevel level, Arg arg, Args... args){
+            instance->log(level,arg,args...);
+        };
+        template <typename Arg, typename... Args>
         void log(Arg arg, Args... args){
-            
+            log(LogLevel::Debug, arg, args...)
         };
-        template <typename... Args>
-        void logd(Args... args){
 
+        template <typename Arg, typename... Args>
+        void logi(Arg arg, Args... args){
+            log(LogLevel::Info, arg, args...);
         };
-        template <typename... Args>
-        void error(Args... args){
+        template <typename Arg, typename... Args>
+        void logd(Arg arg, Args... args){
+            log(LogLevel::Debug, arg, args...);
+        };
+        template <typename Arg, typename... Args>
+        void logw(Arg arg, Args... args){
+            log(LogLevel::Warn, arg, args...);
+        };
+        template <typename Exception, typename Arg, typename... Args>
+        void loge(Exception, Arg arg, Args... args){
+            log(LogLevel::Error, arg, args...);
+        };
 
-        };
-        template <typename... Args>
-        void fatalError(Args... args){
 
-        };
+
+        
 
     }
 
